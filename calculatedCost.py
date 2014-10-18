a9021affc41fa2863b4663e7624a5dac528ff73d
Ulrich Stroetz <ustroetz@gmail.com>
2014-10-18 19:35:44 +0200

    make paths relative to dir

--- calculatedCost.py
+++ calculatedCost.py
@@ -2,13 +2,14 @@ from osgeo import ogr, osr
 import math
 from forestcost import skidding, harvesting
 import rasterstats
-
+import os
 
 def cost_func(stand_wkt, TPA, VPA, SkidDist, Slope):
+    cur_path = os.path.dirname(os.path.realpath(__file__))
 
     ### Input Data
-    slope_raster = 'data/Slope.tif'
-    
+    slope_raster = cur_path + '/data/Slope.tif'
+
     # Reproject
     source = osr.SpatialReference()
     source.ImportFromEPSG(4326)
@@ -26,7 +27,7 @@ def cost_func(stand_wkt, TPA, VPA, SkidDist, Slope):
     area = stand_geom.GetArea()                                         # get area in m2
     area = round(area*0.000247105, 4)                                   # convert to acre and round
     if Slope == None:
-        Slope = rasterstats.raster_stats(stand_wkt, slope_raster,stats=['mean'])  
+        Slope = rasterstats.raster_stats(stand_wkt, slope_raster,stats=['mean'])
         Slope = Slope[0]['mean']                                        # %
 
     if SkidDist == None:
@@ -34,12 +35,10 @@ def cost_func(stand_wkt, TPA, VPA, SkidDist, Slope):
 
     totalVolume = area*VPA                                              # ft3
     totalWeight = totalVolume*0.0195                                    # US short tons (lodepole pine weight)
-    
+
     harvestCostFt3 = harvesting.harvestcost(Slope, SkidDist, TPA, VPA)  # $/ft3
     totalHarvestCost = round(harvestCostFt3*totalVolume)                # $
     harvestCostTon = totalHarvestCost/totalWeight                       # $/ton
-        
+
 
     return Slope, SkidDist, harvestCostTon, totalHarvestCost
-
-
--- estimatedCost.py
+++ estimatedCost.py
@@ -1,7 +1,9 @@
 from osgeo import ogr
+import os
 import rasterstats
 
 def get_zonal_stats(stand_wkt):
-    cost_raster = 'data/CostSurface_final.tif'
-    cost = rasterstats.raster_stats(stand_wkt, cost_raster,stats=['mean'])  
+    cur_path = os.path.dirname(os.path.realpath(__file__))
+    cost_raster = cur_path + '/data/CostSurface_final.tif'
+    cost = rasterstats.raster_stats(stand_wkt, cost_raster,stats=['mean'])
     return cost
